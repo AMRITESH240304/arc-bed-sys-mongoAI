62ff9316a8917938226ff8edeaa3a87b99336baf
AMRITESH240304 <amriteshk778@gmail.com>
2024-10-12 22:40:32 +0530

    fix:port issue fix

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .routes import router
 from .config import settings
 import uvicorn
+import os
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -20,5 +21,10 @@ app.include_router(router)
 def read_root():
     return {"Hello": "Welcome to the MongoDB AI Hackathon"}
 
+@app.head("/")
+def read_head():
+    return {}
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", reload=True)
+    port = settings.PORT or int(os.getenv("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
